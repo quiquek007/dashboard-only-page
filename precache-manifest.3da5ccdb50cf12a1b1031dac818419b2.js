7ff459e4cf7dc6640e4ed352ef0ab6669c46bc30
quiquek007 <quiquek007@gmail.com>
2019-02-14 15:03:55 +0300

    docs: add build version

---
+++ precache-manifest.3da5ccdb50cf12a1b1031dac818419b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9224ae672e69b164b7e21d96c5f7dea",
+    "url": "/static/media/581559031.a9224ae6.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51cf8526773985bd655b",
+    "url": "/static/js/main.51cf8526.chunk.js"
+  },
+  {
+    "revision": "144d8d415ebc20379991",
+    "url": "/static/js/1.144d8d41.chunk.js"
+  },
+  {
+    "revision": "51cf8526773985bd655b",
+    "url": "/static/css/main.82f4934f.chunk.css"
+  },
+  {
+    "revision": "60c411d0b570df42042e6d0a4cb88c45",
+    "url": "/index.html"
+  }
+];
